ac21d737c1daa67e3e21ccdf4d0819df9174162d
Benjamin Klerens <bekle18@student.sdu.dk>
2021-02-18 00:36:48 +0100

    Baseline commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { v4: uuidV4 } = require('uuid')
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+// Redirect from root to RoomID(UUID)
+app.get('/', (req, res) => {
+    res.redirect(`/${uuidV4()}`)
+})
+
+// Upon entering room, render room with UUID
+app.get('/:room', (req, res) => {
+    res.render('room', {roomId: req.params.room})
+})
+
+// On socket connect
+io.on('connection', socket => {
+    // When user joins a room - then broadcast to other users they can connect with each other
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId)
+        socket.to(roomId).broadcast.emit('user-connected', userId)
+        socket.on('disconnect', () => {
+            socket.to(roomId).broadcast.emit('user-disconnect')
+        })
+    })
+})
+
+
+
+server.listen(3000) 
