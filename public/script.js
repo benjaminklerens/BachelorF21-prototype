ad1c020f54df1d08b1f4be817ff6bfd413b98a24
Benjamin Klerens <bekle18@student.sdu.dk>
2021-02-18 01:08:39 +0100

    Peerjs host properties

--- public/script.js
+++ public/script.js
@@ -2,8 +2,8 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myVideo = document.createElement('video')
 const myPeer = new Peer(undefined, {
-    host: '/',
-    port: '3001'
+    host: 'https://prox-vid.herokuapp.com/',
+    port: '9000'
 })
 myVideo.muted = true
 myVideo.controls = true
